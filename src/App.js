de66b7cf97800c4c2563e3da63bcfd719dcf1621
Dré Van den Hooff <dre.vandenhooff@student.hogent.be>
2022-05-27 08:39:19 +0200

    update for new portfolio

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Intro from "./components/Intro";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Skills from "./components/Skills";
+import NewSite from "./components/NewSite";
 
 function App() {
   return (
     <Parallax pages={4} style={{ top: "0", left: "0" }}>
       <ParallaxLayer offset={0} speed={2.5} style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <Intro />
+        <NewSite />
+        {/* <Intro /> */}
       </ParallaxLayer>
 
       <ParallaxLayer offset={1} speed={2} style={{ backgroundColor: "#48d1cc" }} />
@@ -22,8 +24,7 @@ function App() {
           justifyContent: "center",
           alignItems: "center",
           flexDirection: "column",
-        }}
-      >
+        }}>
         <About />
       </ParallaxLayer>
 
@@ -35,8 +36,7 @@ function App() {
           justifyContent: "center",
           alignItems: "center",
           color: "black",
-        }}
-      >
+        }}>
         <Experience />
       </ParallaxLayer>
       <ParallaxLayer offset={3} speed={2} style={{ backgroundColor: "#48d1cc" }} />
@@ -49,10 +49,8 @@ function App() {
           justifyContent: "center",
           alignItems: "center",
           color: "black",
-        }}
-      >
-        <Skills/>
-
+        }}>
+        <Skills />
       </ParallaxLayer>
     </Parallax>
   );
---
+++ src/components/NewSite.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function NewSite() {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <h1>This portfolio is outdated!</h1>
+      <h2 style={{ marginTop: "1em" }}>
+        <a href="https://dre-van-den-hooff.github.io/online-portfolio-v2/">Find the updated version here!</a>
+      </h2>
+    </div>
+  );
+}
